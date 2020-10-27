8d4f305139405c932c0f8bf3a3fc5237b3b44d5b
PRIYANSHU SINGH <55645997+Psingh12354@users.noreply.github.com>
2020-10-27 09:34:05 +0530

    Create Add Two Numbers.cpp

---
+++ Add Two Numbers.cpp
@@ -0,0 +1,17 @@
+// bits/stdc++.h works in linux. 
+// It loads most of the libraries of C++ required.
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        int a,b;
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
+	return 0;
+}
