881be6d64c99c0e854fd4b4a2be37e35d855363b
PRIYANSHU SINGH <55645997+Psingh12354@users.noreply.github.com>
2020-10-30 18:36:31 +0530

    Create Packaging Cupcakes.cpp

---
+++ Packaging Cupcakes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,num;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{
+	    cin>>num;
+	    cout<<(num/2)+1<<endl;
+	}
+	return 0;
+}
