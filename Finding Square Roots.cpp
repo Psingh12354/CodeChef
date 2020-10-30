c51cd98bbb3fba726337a2dc1e8a64755c30256a
PRIYANSHU SINGH <55645997+Psingh12354@users.noreply.github.com>
2020-10-30 13:44:54 +0530

    Create Finding Square Roots.cpp

---
+++ Finding Square Roots.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	    int x;
+	    cin>>x;
+	    int p=sqrt(x);
+	    cout<<p<<endl;
+	}
+	return 0;
+}
