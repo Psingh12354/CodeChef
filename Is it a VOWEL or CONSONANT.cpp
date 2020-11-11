600ea248cb21934e022a24dfdef2b879fc2caa1b
PRIYANSHU SINGH <55645997+Psingh12354@users.noreply.github.com>
2020-11-11 10:52:43 +0530

    Create Is it a VOWEL or CONSONANT.cpp

---
+++ Is it a VOWEL or CONSONANT.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	char n;
+	cin>>n;
+	if(n=='A' or n=='E' or n=='I' or n=='O' or n=='U')
+	{
+	        cout<<"Vowel\n";
+	}
+	else
+	cout<<"Consonant\n";
+	return 0;
+}
